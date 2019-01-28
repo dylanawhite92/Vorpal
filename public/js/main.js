63c273d54d29d423b3e47f02d5805c57e4c130a1
Dylan White <dylanawhite11@gmail.com>
2019-01-27 22:56:30 -0600

    added item icons, made placeholder base price variables

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,26 @@
-// Testing
+$(document).ready(function () {
 
-$("#generator").on("click", function() {
-    alert(value);
+    var commonBasePrice;
+    var uncommonBasePrice;
+    var rareBasePrice;
+    var veryRareBasePrice;
+    var legendaryBasePrice;
+
+    // // Testing generator click
+    // $("#generator").on("click", function() {
+    //     // Value of selected dropdown option
+    //     var value = $("#wealth-select option:selected").text();
+    //     alert(value);
+    // });
+
+    // Testing getting value of select options
+    $("select").on("change", function() {
+        var value = this.value
+
+        console.log(value);
+    });
+
+    function renderInventory() {
+
+    };
 });
-
-$("select").on("change", function() {
-    var value = this.value
-
-    console.log(value);
-})
