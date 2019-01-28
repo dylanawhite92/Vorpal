18885bf40f381f0a87d142412c8ade09b52fd379
Dylan White <dylanawhite11@gmail.com>
2019-01-27 23:50:24 -0600

    started adding switch cases for the different options

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
     var veryRareBasePrice;
     var legendaryBasePrice;
     var shopInventory = [];
+    var wealthValue = $("#wealth-select option:selected").text();
+    var popValue = $("#population-select option:selected").text();
+    var typeValue = $("#type-select option:selected").text();
 
     // Testing generator click
     $("#generator").on("click", function() {
@@ -14,11 +17,11 @@ $(document).ready(function () {
     });
 
     // Testing getting value of select options
-    // $("select").on("change", function() {
-    //     var value = this.value
+    $("select").on("change", function() {
+        var value = this.value
 
-    //     console.log(value);
-    // });
+        console.log(value);
+    });
 
     function renderInventory() {
         var wealthValue = $("#wealth-select option:selected").text();
@@ -28,24 +31,78 @@ $(document).ready(function () {
     };
 
     function populationSize() {
-        switch(value) {
-
+        switch(popValue) {
+            case "Thorp (20-80)":
+            // code
+            break;
+            case "Hamlet (81-400)":
+            // code
+            break;
+            case "Village (401-900)":
+            // code
+            break;
+            case "Small Town (901-2,000)":
+            // code
+            break;
+            case "Large Town (2,001-5,000)":
+            // code
+            break;
+            case "Small City (5,001-12,000)":
+            // code
+            break;
+            case "Large City (12,001-25,000)":
+            // code
+            break;
+            case "Metropolis (25,001+)":
+            // code
+            break;
         }
     };
 
     function townWealth() {
-        switch(value) {
-            
+        switch(wealthValue) {
+            case "Poor":
+            // code
+            break;
+            case "Modest":
+            // code
+            break;
+            case "Comfortable":
+            // code
+            break;
+            case "Affluent":
+            // code
+            break;
+            case "Obscene":
+            // code
+            break;
         }
     }
 
     function shopType() {
-        switch(value) {
-            
+        switch(typeValue) {
+            case "Alchemist":
+            // code
+            break;
+            case "Armorer":
+            // code
+            break;
+            case "Scribe":
+            // code
+            break;
+            case "Trader":
+            // code
+            break;
+            case "Wandwright":
+            // code
+            break;
+            case "Weaponsmith":
+            // code
+            break;
         }
     }
 
     function imageType() {
-        
+
     }
 });
